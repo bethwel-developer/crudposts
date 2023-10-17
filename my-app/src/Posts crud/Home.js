4030f1ed85351dff868cd584e1760e444ca10088
Bethwel Njenga <bethwelnjenga237@gmail.com>
2023-10-17 17:58:17 +0300

    Update Home.js

--- my-app/src/Posts crud/Home.js
+++ my-app/src/Posts crud/Home.js
@@ -3,16 +3,17 @@ import React from 'react'
 import { useState } from 'react'
 import { useEffect } from 'react'
 import { Link } from 'react-router-dom'
+import Create from './Create'
 
 
 
 function Home() {
 
   // fetch all posts
-    const [data, setData] = useState([])
+    const [users, setUsers] = useState([])
     useEffect(()=> {
         axios.get('https://jsonplaceholder.typicode.com/posts')
-        .then(res => setData(res.data))
+        .then(res => setUsers(res.data))
         .catch(err => console.log(err))
     }, [])
 
@@ -25,8 +26,8 @@ function Home() {
       if (res.status !== 200) {
         return;
       } else {
-        setData(
-          data.filter((data) => {
+        setUsers(
+          users.filter((data) => {
             return data.id !== id;
           })
         );
@@ -39,12 +40,44 @@ function Home() {
     }
 
 
+
+    const onAdd = async (title, body) => {
+
+      await fetch("https://jsonplaceholder.typicode.com/posts", {
+        method: "POST",
+        body: JSON.stringify({
+          title: title,
+          body: body,
+        }),
+        headers: {
+          "Content-type": "application/json; charset=UTF-8",
+        },
+      })
+        .then((res) => {
+          if (res.status !== 201) {
+            return;
+          } else {
+            return res.json();
+          }
+
+        })
+        .then((data) => {
+          setUsers((users) => [...users, data]);
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    };
+  
+
+
+
   return (
     <div className='container '>
         <h2 className='mb-3 mt-4 font-bold text-2xl text-center'>CRUD APPLICATION WITH JSONPLACEHOLDER</h2>
-        <button className='focus:outline-none ml-4 mb-4 text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800'>
-        <Link to="/create">Create +</Link>
-            </button>
+        <Create onAdd = {onAdd}/>
+
+       
         <table className='w-full text-sm text-left text-gray-500 dark:text-gray-400'>
             <thead className='text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 '>
                 <tr className='border-b bg-gray-50 font-bold'>
@@ -54,7 +87,9 @@ function Home() {
                 </tr>
             </thead>
             <tbody>
-                {data.map((d, i)=> (
+              
+              
+                {users.map((d, i)=> (
                     <tr className='bg-white border-b dark:bg-gray-800 dark:border-gray-700' key={i}>
                         <td className="px-20 py-4 font-medium text-gray-900  dark:text-white" >{d.title}</td>
                         <td className="px-4 py-0 font-medium text-gray-900  dark:text-white">{d.body}</td>
