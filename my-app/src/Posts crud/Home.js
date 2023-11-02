4abfbd79662e6a3bb0639458abe9610dc3165b4d
Bethwel Njenga <bethwelnjenga237@gmail.com>
2023-11-02 23:09:57 +0300

    Update Home.js

--- my-app/src/Posts crud/Home.js
+++ my-app/src/Posts crud/Home.js
@@ -12,9 +12,9 @@ function Home() {
   // fetch all posts
     const [users, setUsers] = useState([])
     useEffect(()=> {
-        axios.get('https://jsonplaceholder.typicode.com/posts')
-        .then(res => setUsers(res.data))
-        .catch(err => console.log(err))
+          axios.get('https://jsonplaceholder.typicode.com/posts')
+          .then(res => setUsers(res.data))
+          .catch(err => console.log(err))
     }, [])
 
  //delete post
@@ -44,10 +44,10 @@ function Home() {
     const onAdd = async (title, body) => {
 
       await fetch("https://jsonplaceholder.typicode.com/posts", {
-        method: "POST",
-        body: JSON.stringify({
-          title: title,
-          body: body,
+          method: "POST",
+          body: JSON.stringify({
+            title: title,
+            body: body,
         }),
         headers: {
           "Content-type": "application/json; charset=UTF-8",
@@ -81,9 +81,9 @@ function Home() {
         <table className='w-full text-sm text-left text-gray-500 dark:text-gray-400 '>
             <thead className='text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 '>
                 <tr className='border-b bg-gray-50 font-bold'>
-                    <th scope="col" className="px-20 font-bold py-3">title</th>
-                    <th scope="col" className="px-4 py-3">Body</th>
-                    <th  scope="col" className="px-36 py-3">Action</th>
+                      <th scope="col" className="px-20 font-bold py-3">title</th>
+                      <th scope="col" className="px-4 py-3">Body</th>
+                      <th  scope="col" className="px-36 py-3">Action</th>
                 </tr>
             </thead>
             <tbody>
@@ -91,13 +91,13 @@ function Home() {
               
                 {users.map((d, i)=> (
                     <tr className='bg-white border-b dark:bg-gray-800 dark:border-gray-700' key={i}>
-                        <td className="px-20 py-4 font-medium text-gray-900  dark:text-white" >{d.title}</td>
-                        <td className="px-4 py-0 font-medium text-gray-900  dark:text-white">{d.body}</td>
-                        <td>
-                        <Link to={`/read/${d.id}`}>Read</Link>
-                            <button className='focus:outline-none text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 ml-8 mb-2 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-900' 
-                            onClick={e => handleDelete(d.id)}>delete</button>
-                        </td>
+                          <td className="px-20 py-4 font-medium text-gray-900  dark:text-white" >{d.title}</td>
+                          <td className="px-4 py-0 font-medium text-gray-900  dark:text-white">{d.body}</td>
+                          <td>
+                          <Link to={`/read/${d.id}`}>Read</Link>
+                              <button className='focus:outline-none text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 ml-8 mb-2 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-900' 
+                              onClick={e => handleDelete(d.id)}>delete</button>
+                          </td>
                     </tr>
                 ))}
             </tbody>
