83e6b3f1278acb7abb0511110c05a73374da56f1
Bethwel Njenga <bethwelnjenga237@gmail.com>
2023-11-02 23:09:25 +0300

    Update Create.js

--- my-app/src/Posts crud/Create.js
+++ my-app/src/Posts crud/Create.js
@@ -5,10 +5,10 @@ import { Textarea } from 'evergreen-ui';
 
 const Create = ({onAdd}) => {
   const handleSubmit = (e) => {
-    e.preventDefault();
-    onAdd(e.target.title.value,e.target.body.value);
-    e.target.title.value = "";
-    e.target.body.value = "";
+        e.preventDefault();
+        onAdd(e.target.title.value,e.target.body.value);
+        e.target.title.value = "";
+        e.target.body.value = "";
 }
   
   return (
@@ -25,13 +25,13 @@ const Create = ({onAdd}) => {
                    />
                 </div>
                 <div className='mb-8' >
-                    <Textarea className=' text-4xl' placeholder='Enter post content'
-                      name='body'
+                      <Textarea className=' text-4xl' placeholder='Enter post content'
+                        name='body'
                      />
                   
                 </div><br />
-                <button className=' text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800'
-               onSubmit={handleSubmit} >Create post</button>
+                      <button className=' text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800'
+                     onSubmit={handleSubmit} >Create post</button>
             </form>
         </div>
     
