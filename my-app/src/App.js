a614e89ff6c6a278a2a323dfd75a9797986f0850
Bethwel Njenga <bethwelnjenga237@gmail.com>
2023-12-22 22:15:27 +0300

    Update App.js

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -11,7 +11,7 @@ function App() {
             <Route path='/' element={<Home />}></Route>
             <Route path='/create' element={<Create />}></Route>
             <Route path='/update/:id' element={<Update />}></Route>
-            <Route path='/read/:id' element={<Read />}></Route>
+             <Route path='/read/:id' element={<Read />}></Route>
         </Routes>
     </BrowserRouter>
   )
