c1cb33fabb759a28f05f403a6a7676169bbd0aac
Bethwel Njenga <bethwelnjenga237@gmail.com>
2024-01-26 23:59:23 +0300

    Update App.js

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,6 +2,7 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Home from './Posts crud/Home'
 import Read from './Posts crud/Read'
+
 import Update from './Posts crud/Update'
 
 import Create from './Posts crud/Create'
