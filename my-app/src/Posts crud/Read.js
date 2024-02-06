8eaddbff441e7e91b5f783b407bf24f291dbbf84
Bethwel Njenga <bethwelnjenga237@gmail.com>
2024-02-06 05:57:17 +0300

    Update Read.js

--- my-app/src/Posts crud/Read.js
+++ my-app/src/Posts crud/Read.js
@@ -16,7 +16,6 @@ function Read () {
 
   return (
     <div className='  justify-around px-32 py-24 bg-white b'>
-        
             <h5 className='mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white'>Title: {Data.title}</h5>
             <p className='mb-24 font-normal text-xl text-gray-700 dark:text-gray-400'> {Data.body}</p>
             <button className='focus:outline-none mr-4 ml-4 text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800' >
