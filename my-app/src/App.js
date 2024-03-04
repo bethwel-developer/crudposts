987dc9892ac432fd71fbc845832abedb43fafb17
Bethwel Njenga <bethwelnjenga237@gmail.com>
2024-03-04 21:08:10 +0300

    Update App.js

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,4 +1,4 @@
-    import React from 'react'
+import React from 'react'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Home from './Posts crud/Home'
 import Read from './Posts crud/Read'
